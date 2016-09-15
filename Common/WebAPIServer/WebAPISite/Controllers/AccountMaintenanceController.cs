e5729e02bde272b1a95ca4fd2ebbffd7ab3d8239
David Chang <davidchang613@gmail.com>
2016-09-15 12:57:42 -0400

    update

---
+++ Common/WebAPIServer/WebAPISite/Controllers/AccountMaintenanceController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebAPIModel;
+
+namespace WebAPISite.Controllers
+{
+    public class AccountMaintenanceController : Controller
+    {
+        private ApplicationUserManager _userManager;
+
+        // GET: AccountMaintenance
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        [System.Web.Http.AllowAnonymous]
+        public async Task<ActionResult> ConfirmEmail(string userId, string code)
+        {
+
+            if (userId == null || code == null)
+            {
+                return View("Error");
+            }
+
+            var result = await UserManager.ConfirmEmailAsync(userId, code);
+
+            return View(result.Succeeded ? "ConfirmEmail" : "Error");
+        }
+
+        [AllowAnonymous]
+        public ActionResult ResetPassword(string code, string UserId)
+        {
+            return code == null || UserId == null ? View("Error") : View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ResetPassword(ResetPasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var resultconfirmemail = await UserManager.VerifyUserTokenAsync(model.UserId, "ResetPassword", model.Code);
+
+            var user = await UserManager.FindByIdAsync(model.UserId);
+
+            if (user == null || !resultconfirmemail || user.Email.ToLower() != model.Email.ToLower())
+            {
+                // Don't reveal that the user does not exist
+
+                ModelState.AddModelError("err", "User information is not valid. Please try again.");
+                return View();
+            }
+            var result = await UserManager.ResetPasswordAsync(user.Id, model.Code, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("ResetPasswordConfirmation", "AccountConfirm");
+            }
+            AddErrors(result);
+            return View();
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+    }
+}
---
+++ Common/WebAPIServer/WebAPISite/Controllers/ReferenceController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebAPISite.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Reference")]
+    public class ReferenceController : ApiController
+    {
+        public async Task<IHttpActionResult> GetNumber()
+        {
+            int number = await getSomeNumber();
+            return Ok(number);
+        }
+
+        private Task<int> getSomeNumber()
+        {
+            return Task.Run(async () =>
+            {
+                await Task.Delay(10);
+                return 123;
+            });
+        }
+    }
+}
