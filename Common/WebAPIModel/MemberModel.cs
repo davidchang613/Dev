7a0b8faa6052eed8fbdbfbfa09bd6d340d24770e
David Chang <davidchang613@gmail.com>
2016-10-24 23:52:26 -0400

    check in

---
+++ Common/WebAPIModel/MemberModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPIModel
+{
+    public class Member
+    {
+        public string FirstName;
+        public string LastName;
+        public DateTime dob;
+        public Guid status_id;
+    }
+}
